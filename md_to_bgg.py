135b4b67becda526061958a6b9044b83fb4eeb10
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2021-03-23 22:10:16 +0100

    Fixed missing parenthesis.

--- md_to_bgg.py
+++ md_to_bgg.py
@@ -271,7 +271,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=(
             "Convert extended CommonMark markdown to boardgamegeek.com markup."
-            " See https://github.com/lebigot/markdown_to_BGG for details.")
+            " See https://github.com/lebigot/markdown_to_BGG for details."))
     parser.add_argument("input_file", help="Input markdown file")
 
     args = parser.parse_args()
